f3bb81448d7c04fdda9ea3cbfc41acb30bf9799e
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-24 23:01:17 +0530

    Update Function_Program.java

--- Function_Program.java
+++ Function_Program.java
@@ -1,39 +1,19 @@
-
-import java.util.Scanner;
-public class Quadratic {
+public class Sum {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter a: ");
-        double a = s.nextDouble();
-        System.out.print("Enter b: ");
-        double b = s.nextDouble();
-        System.out.print("Enter c: ");
-        double c = s.nextDouble();
+        int[] arr = {1,2,3,-1,-2,-3,4};
+        int sum, count = 0;
 
-        double deltaValue = delta(a,b,c);  
-        System.out.println("Delta value: " + deltaValue);
-
-        double root11 = root1(deltaValue, a, b);    
-        System.out.println("Root 1: " + root11);
-
-        double root21 = root2(deltaValue, a, b);    
-        System.out.println("Root 2: " + root21);
-
+        for (int i=0; i<7; i++) {
+            for (int j=i+1; j<7; j++) {
+                for (int k=j+1; k<7; k++) {
+                    sum = arr[i]+arr[j]+arr[k];
+                    if (sum == 0) {
+                        count++;
+                        System.out.println(arr[i] + "," + arr[j]+ "," + arr[k] + "=" + 0 );
+                    }
+                }
+            }
+        }
+        System.out.println("Count: " + count);
     }
-    
-    private static double delta(double a, double b, double c) {
-        double delta = (b*b)-(4.0*a*c);
-        return delta;
-    }
-
-    private  static double root1(double deltaVal, double a, double b) {
-        double root1 = (-b+Math.sqrt(deltaVal))/(2*a);
-        return root1;
-    }
-
-    private  static double root2(double deltaVal, double a, double b) {
-        double root2 = (b+Math.sqrt(deltaVal))/(2*a);
-        return root2;
-    }
-
 }
