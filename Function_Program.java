bed41b3732692a6fb60b8acfa07b59a53573c44e
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-24 22:58:39 +0530

    Update Function_Program.java

--- Function_Program.java
+++ Function_Program.java
@@ -1,17 +1,43 @@
-import java.util.Scanner;
-
 /**
- * We are finding a distance from origin(0,0) to a certain point (x,y).
+ *  We are finding a roots of quadratic equation ax^2+bx+c.
  */
-public class Distance {
+
+import java.util.Scanner;
+public class Quadratic {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter x coordinate value: ");
-        int x = s.nextInt();
-        System.out.print("Enter y coordinate value: ");
-        int y = s.nextInt();
+        System.out.print("Enter a: ");
+        double a = s.nextDouble();
+        System.out.print("Enter b: ");
+        double b = s.nextDouble();
+        System.out.print("Enter c: ");
+        double c = s.nextDouble();
 
-        double distance = Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
-        System.out.printf("Euclidean Distance is %.2f", distance);
+        double deltaValue = delta(a,b,c);  //call the 'delta' function
+        System.out.println("Delta value: " + deltaValue);
+
+        double root11 = root1(deltaValue, a, b);     // call the 'root1' function
+        System.out.println("Root 1: " + root11);
+
+        double root21 = root2(deltaValue, a, b);     // call the 'root2' function
+        System.out.println("Root 2: " + root21);
+
     }
+
+    // define a function 'delta', it will return delta value
+    private static double delta(double a, double b, double c) {
+        double delta = (b*b)-(4.0*a*c);
+        return delta;
+    }
+
+    private  static double root1(double deltaVal, double a, double b) {
+        double root1 = (-b+Math.sqrt(deltaVal))/(2*a);
+        return root1;
+    }
+
+    private  static double root2(double deltaVal, double a, double b) {
+        double root2 = (b+Math.sqrt(deltaVal))/(2*a);
+        return root2;
+    }
+
 }
